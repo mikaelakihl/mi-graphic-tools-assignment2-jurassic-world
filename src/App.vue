a32b55cd98b3898a6b9ab87d344bf8cc9eb1720c
mikaelakihl <mikaela.kihl@medieinstitutet.se>
2025-03-05 15:58:01 +0100

    feat: added classes and styling at router links

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,15 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <header>
 
-    <div class="wrapper"> 
+    <div class="wrapper">
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/tickets">Tickets</RouterLink>
-        <RouterLink to="/cast">Cast</RouterLink>
+      <h2>Menu</h2>
+
+      <nav class="nav-links">
+        <RouterLink class="router-link" to="/">Home</RouterLink>
+        <RouterLink class="router-link" to="/about">About</RouterLink>
+        <RouterLink class="router-link" to="/tickets">Tickets</RouterLink>
+        <RouterLink class="router-link" to="/cast">Cast</RouterLink>
       </nav>
     </div>
   </header>
@@ -21,5 +23,29 @@ import { RouterLink, RouterView } from 'vue-router'
 </template>
 
 <style lang="scss" scoped>
+
+.wrapper{
+  background-color: black;
+  width: 80%;
+  height: 100vh;
+  color: white;
+}
+
+.nav-links{
+
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 10px;
+
+
+
+
+}
+
+.router-link{
+  color: white;
+  text-decoration: none;
+  border-bottom: solid white 1px;
+}
 
 </style>

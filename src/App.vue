a619b1500b71629a3ffcb6f31811ae87960c51fc
CatrinTQ <91636034+CatrinTQ@users.noreply.github.com>
2025-03-06 17:22:13 +0100

    feat: add fotter closes Footer - Mobile #15 closes Footer - Tablet/Desktop #9

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import AppFooter from './components/molecules/AppFooter.vue'
 </script>
 
 <template>
@@ -11,10 +12,11 @@ import { RouterLink, RouterView } from 'vue-router'
         <RouterLink to="/tickets">Tickets</RouterLink>
         <RouterLink to="/cast">Cast</RouterLink>
       </nav>
+
     </div>
   </header>
-
   <RouterView />
+  <AppFooter />
 </template>
 
 <style lang="scss" scoped></style>

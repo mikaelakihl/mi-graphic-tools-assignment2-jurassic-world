dff8b7b0024c306513d7c59d240060d2c0a8dbe6
Osklundev <Oskarlundberg@hotmail.com>
2025-03-07 10:48:47 +0100

    feat: Added TheButton to atoms and added it to TicketView for example on how to use it.

--- src/views/TicketView.vue
+++ src/views/TicketView.vue
@@ -1,8 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TheButton from '@/components/atoms/TheButton.vue';
+</script>
 
 <template>
   <div class="ticket">
     <h1>This is the Tickets page</h1>
+    <TheButton button-text="Buy now"/> <!-- Placeholder example on how to use "TheButton" -->
   </div>
 </template>
 

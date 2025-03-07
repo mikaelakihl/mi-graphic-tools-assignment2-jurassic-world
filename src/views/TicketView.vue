0b8698a8b7cbee6c6511d070bce95c248dbf1f31
Osklundev <Oskarlundberg@hotmail.com>
2025-03-07 11:22:27 +0100

    Feat: Added atom components for Increment & Decrement btns

--- src/views/TicketView.vue
+++ src/views/TicketView.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
 import BookingCard from '@/components/molecules/BookingCard.vue';
+import IncrementBtn from '@/components/atoms/IncrementButton.vue';
+import DecrementButton from '@/components/atoms/DecrementButton.vue';
 </script>
 
 <template>
   <div class="ticket">
     <BookingCard />
     <h1>This is the Tickets page</h1>
+    <IncrementBtn button-text="+"/>
+    <DecrementButton button-text="-" />
   </div>
 </template>
 

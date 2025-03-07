6198a08193b13379dbcf3f4f7e301176f0e310ae
David Kjellstrand <david.kjellstrand@gmail.com>
2025-03-07 14:10:37 +0100

    feat: add simple html and styling for order form component in ticketview

---
+++ src/components/molecules/OrderForm.vue
@@ -0,0 +1,102 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <section class="order-form">
+    <form>
+      <div class="form-container">
+        <label>
+          <span>Full Name</span>
+          <input id="nameInput" type="text" name="name" placeholder="Enter your full name" required>
+        </label>
+      </div>
+      <div class="form-container">
+        <label class="ticket-amount-container">
+          <span>Tickets</span>
+          <div>
+            <button>-</button>
+            <span>0</span>
+            <button>+</button>
+          </div>
+        </label>
+      </div>
+      <div class="form-container">
+        <label>
+          <span>Email</span>
+          <input id="emailInput" type="email" name="email" placeholder="Enter your email address" required>
+        </label>
+      </div>
+      <div class="form-container">
+        <label>
+          <span>Phone (Optional)</span>
+          <input id="phoneInput" type="tel" name="phone" placeholder="Enter your phone number (optional)">
+        </label>
+      </div>
+      <div class="form-container">
+        <label class="terms-container">
+          <input id="termsCheckbox" type="checkbox" name="terms" required>
+          <span>I agree to the terms and conditions</span>
+        </label>
+      </div>
+      <div class="form-container">
+        <button>Proceed</button>
+        <button>Cancel</button>
+      </div>
+    </form>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+  section {
+    width: 100vw;
+    display: flex;
+    form {
+      background-color: rgb(219, 131, 131);
+      width: 100%;
+      height: 100%;
+      display: grid;
+      grid-template-columns: repeat(4, 1fr);
+      .form-container {
+        grid-column: span 4; // Most span 4, below are exeptions
+        width: 100%;
+        padding: 1rem;
+        gap: 1rem;
+        background-color: rgb(0, 0, 0);
+        label {
+          width: 100%;
+          display: flex;
+          flex-direction: column;
+          span {
+            color: rgb(255, 255, 255);
+            // padding-bottom: .3rem;
+          }
+        }
+        .terms-container {
+          flex-direction: row;
+          gap: 1rem;
+          font-size: 1.2rem;
+        }
+        .ticket-amount-container {
+          align-items: center;
+          height: 100%;
+          div {
+            display: flex;
+            width: 100%;
+            height: 100%;
+            justify-content: space-between;
+            align-items: center;
+          }
+        }
+      }
+      .form-container:nth-child(1),
+      .form-container:nth-child(2) {
+        grid-column: span 2;
+      }
+      .form-container:nth-child(6) {
+        display: flex;
+        justify-content: space-between;
+      }
+    }
+  }
+</style>
--- src/views/TicketView.vue
+++ src/views/TicketView.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import BookingCard from '@/components/molecules/BookingCard.vue';
+import OrderForm from '@/components/molecules/OrderForm.vue';
 </script>
 
 <template>
   <div class="ticket">
     <BookingCard />
     <h1>This is the Tickets page</h1>
+    <OrderForm />
   </div>
 </template>
 
